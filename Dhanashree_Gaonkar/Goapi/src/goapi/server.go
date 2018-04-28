948b1f078c6a799cf4da65c676c9dde3e27a512c
Dhanashree Gaonkar <dgaonkar17@gmail.com>
2018-04-27 17:47:40 -0700

    Update server.go

--- Dhanashree_Gaonkar/Goapi/src/goapi/server.go
+++ Dhanashree_Gaonkar/Goapi/src/goapi/server.go
@@ -1,30 +1,35 @@
+/*
+	 API in Go (Version 3)
+	Uses MongoDB 
+	(For use with Kong API Key)
+*/
+
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"github.com/codegangsta/negroni"
-	"github.com/gorilla/mux"
-	"github.com/mediocregopher/radix.v2/redis"
-	"github.com/unrolled/render"
-	"gopkg.in/mgo.v2"
-	"gopkg.in/mgo.v2/bson"
 	"log"
-	"math/rand"
 	"net/http"
+	"encoding/json"
 	"strconv"
+	//"goji.io/pat"
+	"github.com/codegangsta/negroni"
+	//"github.com/streadway/amqp"
+	"github.com/gorilla/mux"
+	"github.com/unrolled/render"
+	//"github.com/satori/go.uuid"
+	"gopkg.in/mgo.v2"
+    "gopkg.in/mgo.v2/bson"
 )
 
-var redis_connect = "192.168.99.100:6379"
-var mongodb_server1 = "192.168.99.100:27017"
-var mongodb_server2 = "192.168.99.100:27018"
-var mongodb_server3 = "192.168.99.100:27019"
-var mongodb_database = "cmpe281"
-var mongodb_collection = "redistest"
+// MongoDB Config
+var mongodb_server = "mongodb://54.193.23.61:27017/"
+var mongodb_database = "admin"
+var mongodb_collection = "admin"
 
-var servers = []string{mongodb_server1, mongodb_server2, mongodb_server3}
 
-// Configure and return a server
+
+// NewServer configures and returns a Server.
 func NewServer() *negroni.Negroni {
 	formatter := render.New(render.Options{
 		IndentJSON: true,
@@ -38,43 +43,191 @@ func NewServer() *negroni.Negroni {
 
 // API Routes
 func initRoutes(mx *mux.Router, formatter *render.Render) {
-  //define routine here
-	
-	mx.HandleFunc("/getid/{item_id}", getHandler(formatter)).Methods("GET")
-	mx.HandleFunc("/postitem", postHandler(formatter)).Methods("POST")
+	mx.HandleFunc("/ping", pingHandler(formatter)).Methods("GET")
+	mx.HandleFunc("/gumball/{id}", gumballHandler(formatter)).Methods("GET")
+	mx.HandleFunc("/gumball", gumballUpdateHandler(formatter)).Methods("PUT")
+	mx.HandleFunc("/gumball", gumballNewOrderHandler(formatter)).Methods("POST")
+	mx.HandleFunc("/gumball/{id}", gumballDeleteHandler(formatter)).Methods("DELETE")
 }
-
 
 // Helper Functions
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+		panic(fmt.Sprintf("%s: %s", msg, err))
+	}
+}
 
-func getFromMongo(session *mgo.Session, serialNumber string) User {
+// API Ping Handler
+func pingHandler(formatter *render.Render) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		formatter.JSON(w, http.StatusOK, struct{ Test string }{"API version 1.0 alive!"})
+	}
+}
 
-	var result User
-	//get from mongo
-	if session != nil {
-		c := session.DB(mongodb_database).C(mongodb_collection)
-		err := c.Find(bson.M{"serialnumber": serialNumber}).One(&result)
-		if err != nil {
-			//could not find in mongo (inserting into mongo for now. TODO: Make proper)
-			// c.Insert(bson.M{"SerialNumber": "1", "Name": "Test"})
-			fmt.Println("Some Error in Get, maybe data is not present")
+// API  Handler --------------- GET ------------------
+func gumballHandler(formatter *render.Render) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+	    //var m gumballMachine
+		
+		vars:=mux.Vars(req)
+		id,err1 := strconv.Atoi(vars["id"])
+	
+		if err1 != nil {
+			fmt.Println(err1)
 		}
+		
+		session, err := mgo.Dial(mongodb_server)
+        if err != nil {
+                panic(err)
+        }
+        defer session.Close()
+        session.SetMode(mgo.Monotonic, true)
+        c := session.DB(mongodb_database).C(mongodb_collection)
+        var result bson.M
+        err = c.Find(bson.M{"Id" : id}).One(&result)
+        if err != nil {
+                log.Fatal(err)
+        }
+        fmt.Println("Gumball Machine:", result )
+		formatter.JSON(w, http.StatusOK, result)
 	}
-	return result
-
 }
 
-func getSession(mongodb_bal_server string) *mgo.Session {
-	// Connect to mongo cluster
-	//mongodb_bal_server := Balance()
-	fmt.Println("mongo connecting to " + mongodb_bal_server)
-	s, err := mgo.Dial(mongodb_bal_server)
-
-	if err == nil {
-		s.SetMode(mgo.Monotonic, true)
+// API Update Inventory ----------- PUT --------------
+func gumballUpdateHandler(formatter *render.Render) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+    	var m gumballMachine
+    	_ = json.NewDecoder(req.Body).Decode(&m)		
+    	fmt.Println("Update Gumball Inventory To: ", m.CountGumballs)
+		session, err := mgo.Dial(mongodb_server)
+        if err != nil {
+                panic(err)
+        }
+        defer session.Close()
+        session.SetMode(mgo.Monotonic, true)
+        c := session.DB(mongodb_database).C(mongodb_collection)
+        query := bson.M{"Id" : m.Id}
+        change := bson.M{"$set": bson.M{ "CountGumballs" : m.CountGumballs, "SerialNumber":m.SerialNumber,"ModelNumber":m.ModelNumber}}
+        err = c.Update(query, change)
+        if err != nil {
+                log.Fatal(err)
+        }
+       	var result bson.M
+        err = c.Find(bson.M{"Id" : m.Id}).One(&result)
+        if err != nil {
+                log.Fatal(err)
+        }        
+        fmt.Println("Gumball Machine:", result )
+		formatter.JSON(w, http.StatusOK, result)
 	}
+}
 
-	return s
+// --------------------- POST ----------------------------
+func gumballNewOrderHandler(formatter *render.Render) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		var m gumballMachine
+    	_ = json.NewDecoder(req.Body).Decode(&m)		
+    	fmt.Println("Insert into Inventory ")
+		session, err := mgo.Dial(mongodb_server)
+        if err != nil {
+                panic(err)
+        }
+        defer session.Close()
+        session.SetMode(mgo.Monotonic, true)
+        c := session.DB(mongodb_database).C(mongodb_collection)
+        query := bson.M{"Id" : m.Id, "CountGumballs" : m.CountGumballs, "ModelNumber" : m.ModelNumber, "SerialNumber" : m.SerialNumber}
+        err = c.Insert(query)
+        if err != nil {
+                log.Fatal(err)
+        }
+       	var result bson.M
+        err = c.Find(bson.M{"Id" : m.Id}).One(&result)
+        if err != nil {
+                log.Fatal(err)
+        }
+		formatter.JSON(w, http.StatusOK, result)
+	}
+}
+
+// ------------------ Delete ---------------------
+func gumballDeleteHandler(formatter *render.Render) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		var m gumballMachine
+    	_ = json.NewDecoder(req.Body).Decode(&m)
+		vars := mux.Vars(req)
+		id, err1 := strconv.Atoi(vars["id"])
+		if err1 != nil {
+			fmt.Println(err1)
+		}
+		session, err := mgo.Dial(mongodb_server)
+        if err != nil {
+                panic(err)
+        }
+		fmt.Println("Gumball Machine id:", id)
+        defer session.Close()
+        session.SetMode(mgo.Monotonic, true)
+        c := session.DB(mongodb_database).C(mongodb_collection)
+        var result bson.M
+        err = c.Remove(bson.M{"Id" : id})
+        if err != nil {
+                log.Fatal(err)
+        }
+        fmt.Println("Gumball Machine:", result)
+		formatter.JSON(w, http.StatusOK, result)
+	}
 }
 
 
+/*
+
+	-- RabbitMQ Setup
+
+	http://localhost:8080
+
+	-- RabbitMQ Create Queue:  
+
+		Queue Name: gumball
+		Durable:	no
+
+	-- Gumball MongoDB Create Database
+
+		Database Name: cmpe281
+		Collection Name: gumball
+
+  	-- Gumball MongoDB Collection (Create Document) --
+
+    db.gumball.insert(
+	    { 
+	      Id: 1,
+	      CountGumballs: NumberInt(202),
+	      ModelNumber: 'M102988',
+	      SerialNumber: '1234998871109' 
+	    }
+	) ;
+
+    -- Gumball MongoDB Collection - Find Gumball Document --
+
+    db.gumball.find( { Id: 1 } ) ;
+
+    {
+        "_id" : ObjectId("54741c01fa0bd1f1cdf71312"),
+        "Id" : 1,
+        "CountGumballs" : 202,
+        "ModelNumber" : "M102988",
+        "SerialNumber" : "1234998871109"
+    }
+
+    -- Gumball MongoDB Collection - Update Gumball Document --
+
+    db.gumball.update( 
+        { Dd: 1 }, 
+        { $set : { CountGumballs : NumberInt(10) } },
+        { multi : false } 
+    )
+
+    -- Gumball Delete Documents
+
+    db.gumball.remove({})
+
+ */
