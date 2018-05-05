35391bd0eafa2cacf0b7777341aec91d1b123124
noopurjoshi <noopur2393@gmail.com>
2018-05-04 22:31:14 -0700

    final struct changes

--- noopurjoshi/src/goapi/types.go
+++ noopurjoshi/src/goapi/types.go
@@ -1,8 +1,38 @@
 package main
 
-type gumballMachine struct {
-	Id             	int 	
-	CountGumballs   int    	
-	ModelNumber 	string	    
-	SerialNumber 	string	
+type user struct {
+	Email       string 	
+	Password	string    	
+	UserId		string
+}
+
+type item struct {
+	Name		string `bson:"Name"`
+	Description	string	`bson:"Description"`
+	Id			string	`bson:"Id"`
+	Quantity	int		`bson:"Quantity"`
+	Price		int		`bson:"Price"`
+}
+
+type orderDetails struct {
+	OrderId     string 		`bson:"OrderId"`
+	Items		item    	`bson:"Items"`
+	UserId		string		`bson:"UserId"`
+}
+
+type cart struct {
+	UserId 	string	`bson:"UserId"`
+	CartId	string	`bson:"CartId"`
+	Status	string	`bson:"Status"`
+	Orders 	[]orderDetails	`bson:"Orders"`
+}
+
+type paymentDetails struct {
+	PaymentId   	string 	
+	OrderId			string    	
+	UserId			string
+	FullName		string
+	Phone			string
+	PaymentType		string
+	TotalPrice		int
 }
